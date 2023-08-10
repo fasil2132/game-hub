cfa7f316f37763e90de36e089cb71a72a56267b5
fasil2132 <fasilzegeye@gmail.com>
2023-08-10 15:44:57 +0300

    Show Loading Skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,13 +11,15 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10}>
-      <Image src={getCroppedImageUrl(game.background_image)} overflow='hidden' />
+    <Card width={"300px"} borderRadius={10} overflow="hidden">
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize={'2xl'}>{game.name}</Heading>
-        <HStack justifyContent='space-between'>
-        <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
-        <CriticScore score={game.metacritic}/>
+        <Heading fontSize={"2xl"}>{game.name}</Heading>
+        <HStack justifyContent="space-between">
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
         </HStack>
       </CardBody>
     </Card>
