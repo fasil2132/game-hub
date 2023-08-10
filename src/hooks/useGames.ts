c880f6856699e19f0df6f51d5ad4ada83ed53875
fasil2132 <fasilzegeye@gmail.com>
2023-08-10 18:10:25 +0300

    Filter games by genre

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 import useData from "./UseData";
+import { Genre } from "./useGenre";
 
 export interface Platform {
     id: number;
@@ -22,7 +23,7 @@ export interface Game {
   //   results: Game[];
   // }
 
-  const useGames = () => useData<Game>("/games");
+  const useGames = (selectedGenre: Genre | null) => useData<Game>("/games", {params: {genres: selectedGenre?.id}}, [selectedGenre?.id]);
 // const useGames = () => {
   
 //     const [games, setGames] = useState<Game[]>([]);
