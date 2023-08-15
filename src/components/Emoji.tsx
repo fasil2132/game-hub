4c7c0e783da54db81c8d787e8d28c25228032566
fasil2132 <fasilzegeye@gmail.com>
2023-08-15 20:27:53 +0300

    Add emojis

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,20 @@
+import { Image, ImageProps } from "@chakra-ui/react";
+import bullsEye from "../assets/bullseye.webp";
+import meh from "../assets/meh.webp";
+import thumbsUp from "../assets/thumbs-up.webp";
+
+interface Props {
+  rating: number;
+}
+
+const Emoji = ({ rating }: Props) => {
+  if (rating < 3) return null;
+  const emojiMap: {[key: number]: ImageProps} = {
+    3: {src: meh, alt: 'meh'},
+    4: {src: thumbsUp, alt: 'recommended'},
+    5: {src: bullsEye, alt: 'exceptional'}
+  }
+  return <Image {...emojiMap[rating]} boxSize='25px' marginTop={1} />
+};
+
+export default Emoji;
